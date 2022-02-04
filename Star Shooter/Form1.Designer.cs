96d95eda978c1e6a4891a8e050fe8c459af3ecfe
Hawic <moszat6688@gmail.com>
2022-02-04 12:27:00 +0100

    first commit

---
+++ Star Shooter/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Star_Shooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pBSpaceShip = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBSpaceShip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.pBSpaceShip);
+            this.panel1.Location = new System.Drawing.Point(-1, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1490, 609);
+            this.panel1.TabIndex = 0;
+            // 
+            // pBSpaceShip
+            // 
+            this.pBSpaceShip.Image = ((System.Drawing.Image)(resources.GetObject("pBSpaceShip.Image")));
+            this.pBSpaceShip.Location = new System.Drawing.Point(13, 204);
+            this.pBSpaceShip.Name = "pBSpaceShip";
+            this.pBSpaceShip.Size = new System.Drawing.Size(120, 119);
+            this.pBSpaceShip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pBSpaceShip.TabIndex = 0;
+            this.pBSpaceShip.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lifes:  3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(1233, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score: ";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1404, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(1404, 54);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer2.TabIndex = 3;
+            this.axWindowsMediaPlayer2.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.btnExit);
+            this.panel2.Controls.Add(this.btnPlayAgain);
+            this.panel2.Location = new System.Drawing.Point(635, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(232, 156);
+            this.panel2.TabIndex = 4;
+            this.panel2.Visible = false;
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPlayAgain.Location = new System.Drawing.Point(0, 89);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(77, 64);
+            this.btnPlayAgain.TabIndex = 0;
+            this.btnPlayAgain.Text = "Play again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnExit.Location = new System.Drawing.Point(155, 89);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(77, 64);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(65, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Score:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1491, 692);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBSpaceShip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pBSpaceShip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnPlayAgain;
+    }
+}
+
---
+++ Star Shooter/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Star_Shooter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+       
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            { MoveUp(); }
+            else if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            { MoveDown(); }
+
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            { MoveForeward(); }
+
+            else if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            { MoveBack(); }
+            else if (e.KeyCode == Keys.Space)
+            { Fire(); }
+        }
+       
+        private void MoveUp()
+        {
+            Point spacePoz = pBSpaceShip.Location;
+            if (spacePoz.Y>20)
+            {
+                pBSpaceShip.Top -= 10;
+                pBSpaceShip.ImageLocation = @"c:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\ship\left.png";
+            }
+           
+            
+        }
+        private void MoveDown()
+        {
+            Point spacePoz = pBSpaceShip.Location;
+            if (spacePoz.Y < 469)
+            {
+                pBSpaceShip.Top += 10;
+                pBSpaceShip.ImageLocation = @"c:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\ship\right.png";
+            }
+        }
+        private void MoveForeward()
+        {
+            Point spacePoz = pBSpaceShip.Location;
+            if (spacePoz.X < 1350)
+            { pBSpaceShip.Left += 10; }
+        }
+        private void MoveBack()
+        {
+            Point spacePoz = pBSpaceShip.Location;
+            if (spacePoz.X > 20)
+            { pBSpaceShip.Left -= 10; }
+        }
+
+        private void Fire()
+        {
+            axWindowsMediaPlayer2.URL = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\ship\fire.mp3";
+            axWindowsMediaPlayer2.Ctlcontrols.play();
+            Point firePoz = pBSpaceShip.Location;
+            firePoz.X = firePoz.X + 120;
+            firePoz.Y = firePoz.Y + (109/2);
+            PictureBox fire = new PictureBox();
+            fire.ImageLocation = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\ship\fire.png";
+            fire.Height = 15;
+            fire.Width = 20;
+            fire.Name = "fire";
+            fire.Location=firePoz;
+            panel1.Controls.Add(fire);
+            axWindowsMediaPlayer2.Ctlcontrols.stop();
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up || e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                pBSpaceShip.ImageLocation= @"c:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\ship\ship.png";
+            }   
+        }
+        
+        private void CreateAliens()
+        {
+            Random rnd = new Random();
+            int randomNumber=rnd.Next(0, 5);
+            int randomPozition= rnd.Next(0, 5);
+            PictureBox alien = new PictureBox();
+            alien.Name = "Alien";
+            alien.Width = 75;
+            alien.Height = 75;
+           
+
+            switch (randomNumber)
+            {
+                case 1:
+                    alien.ImageLocation = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\Aliens\1.png";
+                    break;
+                case 2:
+                    alien.ImageLocation = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\Aliens\2.png";
+                    break;
+                case 3:
+                    alien.ImageLocation = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\Aliens\3.png";
+                    break;
+                case 4:
+                    alien.ImageLocation = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\Aliens\4.png";
+                    break;
+            }
+            switch (randomPozition)
+            {
+                case 1:
+                    alien.Location = new Point(panel1.Width, 100);
+                    panel1.Controls.Add(alien);
+                    break;
+
+                case 2:
+                    alien.Location = new Point(panel1.Width, 200);
+                    panel1.Controls.Add(alien);
+                    break;
+                case 3:
+                    alien.Location = new Point(panel1.Width, 300);
+                    panel1.Controls.Add(alien);
+                    break;
+
+                case 4:
+                    alien.Location = new Point(panel1.Width, 400);
+                    panel1.Controls.Add(alien);
+                    break;
+
+            }
+            alien.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+        }
+        private void Stars()
+        {
+            PictureBox stars = new PictureBox();
+            stars.Name = "Stars";
+            stars.ImageLocation = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\star\star.png";
+            stars.Width = 10;
+            stars.Height = 10;
+            stars.SizeMode = PictureBoxSizeMode.StretchImage;
+            Random rnd = new Random();
+            int starLocation = rnd.Next(10,panel1.Height-10);
+            stars.Location = new Point(panel1.Width, starLocation);
+            stars.BackColor = Color.Transparent;
+            panel1.Controls.Add(stars);
+        }
+
+        int counter = 0;
+        int score = 0;
+        int life = 3;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            for (int i = 0; i < panel1.Controls.Count; i++)
+            {
+                PictureBox pictures = (PictureBox)panel1.Controls[i];
+                if (pictures.Name == "fire")
+                {
+                    if (pictures.Location.X > panel1.Width)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pictures.Left += 10;
+                    }
+                    for (int j = 0; j < panel1.Controls.Count; j++)
+                    {
+                        PictureBox aliensPB = (PictureBox)panel1.Controls[j];
+                        if (aliensPB.Name=="Alien")
+                        {
+                            if (pictures.Location.X + pictures.Width > aliensPB.Location.X  )
+                            {
+                                if (pictures.Location.Y + pictures.Height > aliensPB.Location.Y  && pictures.Location.Y + pictures.Height < aliensPB.Location.Y + aliensPB.Height)
+                                {
+                                    panel1.Controls.RemoveAt(j);
+                                    panel1.Controls.Remove(pictures);
+                                    score++;
+                                    label2.Text = "Score: " + score.ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else if (pictures.Name == "Alien")
+                {
+                    if (pictures.Location.X <= 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+
+                    else
+                    {
+                        pictures.Left -= 10;
+                        if (pBSpaceShip.Location.X + pBSpaceShip.Width > pictures.Location.X  && pBSpaceShip.Location.X < pictures.Location.X)
+                        {
+                            if (pBSpaceShip.Location.Y < pictures.Location.Y && pBSpaceShip.Location.Y + pBSpaceShip.Height > pictures.Location.Y)
+                            {
+                                panel1.Controls.Remove(pictures);
+                                life--;
+                                label1.Text = "Life: "+ life;
+                                if (life == 0)
+                                {
+                                    axWindowsMediaPlayer2.URL = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\sounds\explode.mp3";
+                                    axWindowsMediaPlayer2.Ctlcontrols.play();
+                                    timer1.Stop();
+                                    axWindowsMediaPlayer2.Ctlcontrols.play();
+                                    axWindowsMediaPlayer1.Ctlcontrols.stop();
+                                    panel2.Visible = true;
+                                    label3.Text = "Score: " + Environment.NewLine+score;
+                                }
+                                
+                            }
+                        }
+                    }
+
+                   
+
+                }
+
+                else if (pictures.Name == "Stars")
+                {
+                    if (pictures.Location.X <= 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+
+                    else
+                    {
+                        pictures.Left -= 3;
+                    }
+                }
+            }
+            if (counter == 25)
+            {
+                CreateAliens();
+                counter = 0;
+            }
+            else if (counter == 1)
+            { Stars(); }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            axWindowsMediaPlayer1.URL = @"C:\Users\Hawic\source\repos\Star Shooter\Star Shooter\Resources\sounds\background.mp3";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms[0] == this)
+            { Application.Restart(); }
+        }
+    }
+}
